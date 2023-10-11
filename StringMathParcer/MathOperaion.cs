8e4d9e4432d4376226de5c9a8a75d3bb88aaa1aa
Alim-Lyukmanov <78745247+Alim-Lyukmanov@users.noreply.github.com>
2023-10-12 00:50:34 +0300

    Added MathOperation class

---
+++ StringMathParcer/MathOperaion.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+public class MathOperation
+{
+	private double first;
+	private double second;
+	private string operation;
+
+
+	public double First { get=>first; set=>first = value; }
+	public double Second { get =>second; set=>second = value; }
+	public string Operation { get => operation; set => operation = value; }
+
+	public MathOperation(string strMathOperation)
+	{
+		first = double.Parse(Regex.Matches(strMathOperation, @"\d+")[0].ToString());
+        second = double.Parse(Regex.Matches(strMathOperation, @"\d+")[1].ToString());
+        operationSymbol = Regex.Match(strMathOperation, @"(\/|\*|\+|\-)").ToString()
+    }
+
+    private double Division()
+    {
+        return first/second;
+    }
+
+    private int Multiplication()
+    {
+        return first * second;
+    }
+
+    private int Difference()
+    {
+        return first-second;
+    }
+
+    private int Sum()
+    {
+        return first+second;
+    }
+
+
+
+}
